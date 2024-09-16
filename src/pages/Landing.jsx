456f2c25da6c66fc0c7974a0e68aad2f4710fabe
petergambo <petegambo@gmail.com>
2024-09-16 13:59:46 +0100

    Fixed images

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -55,12 +55,12 @@ const EcommercePage = () => {
 
                 {/* Main Section */}
 
-                <div className="flex mt-4 gap-2 mx-[100px]">
+                <div className="flex flex-col md:flex-row mt-4 gap-2 md:mx-[100px]">
                     {/* Categories Column */}
 
-                    <div className="w-2/12 space-y-4 h-[390px] overflow-y-auto">
+                    <div className="w-full md:w-2/12 space-y-4 md:h-[390px] overflow-y-auto">
                         <WhiteInGrayContainer rounded={true}>
-                            <ul>
+                            <ul className='flex flex-row gap-8 md:gap-2 items-center md:items-start md:flex-col'>
                                 {categories.map((item) => {
                                     return (
                                         <li className='p-[6px]'>
@@ -78,7 +78,7 @@ const EcommercePage = () => {
                         </WhiteInGrayContainer>
                     </div>
                     {/* Carousel Column */}
-                    <div className="w-8/12 h-96 relative">
+                    <div className="md:w-8/12 h-96 relative">
                         {/* Carousel Image */}
                         <img
                             src="/landing-page/slider-1.png"
@@ -102,7 +102,7 @@ const EcommercePage = () => {
                     </div>
 
                     {/* Contact Column */}
-                    <div className="w-2/12 flex flex-col space-y-4">
+                    <div className="w-2/12 hidden md:flex flex-col space-y-4">
                         <div className="flex flex-col justify-center items-center h-48">
                             <WhiteInGrayContainer rounded={true}>
                                 <div className="flex items-center space-x-2">
@@ -123,7 +123,7 @@ const EcommercePage = () => {
                 </div>
 
                 {/* Image Containers */}
-                <div className="grid grid-cols-6 gap-4 mt-4 mx-[100px]">
+                <div className="grid md:grid-cols-6 gap-4 mt-4 md:mx-[100px]">
                     {[{ name: 'Buy Now', src: "/landing-page/masa2.jpg" }, { name: 'New Vendors', src: "/landing-page/new-arrival.png" }, { name: 'Shop Local', src: "/landing-page/market.jpg" }, { name: 'Electronics', src: "/landing-page/ds.jpg" }, { name: 'Top Vendors', src: "/landing-page/unnamed (1).jpg" }, { name: 'Special Offers', src: "/landing-page/lcmrk.jpg" }].map((item, index) => (
                         <div key={index} className="relative h-56">
                             <img
@@ -139,7 +139,7 @@ const EcommercePage = () => {
                 </div>
             </div>
 
-            <div className='mx-[100px]'>
+            <div className='md:mx-[100px]'>
                 <TopVendorsCard preData={[]} title={"All Vendors"} />
             </div>
 
